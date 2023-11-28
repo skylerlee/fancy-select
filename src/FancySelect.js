bb49fb79969d5b7ca5be45d0aa2c51c97f7c7ec9
skylerlee <skyler.ac.lee@gmail.com>
2023-11-29 03:39:23 +0800

    feat: update

--- src/FancySelect.js
+++ src/FancySelect.js
@@ -5,19 +5,33 @@ function FancySelect(props) {
   const { options = [] } = props;
   const [value = options[0]?.value, onChange] = useControllableValue(props);
 
-  const handleClick = (e, idx, opt) => {
+  const handleItemClick = (e, idx, opt) => {
     if (opt !== undefined) {
       onChange(opt.value);
     }
   };
 
+  const handleWheel = (e) => {
+    const ratio = e.deltaY / 100;
+    let step = 0;
+    if (ratio > 1) {
+      step = 1;
+    } else if (ratio < -1) {
+      step = -1;
+    }
+    const currentIdx = options.findIndex((opt) => opt.value === value);
+    const nextIdx = (currentIdx + step) % options.length;
+    const nextOpt = options[nextIdx];
+    onChange(nextOpt?.value);
+  };
+
   return (
-    <div className="FancySelect">
+    <div className="FancySelect" onWheel={handleWheel}>
       {options.map((opt, idx) => (
         <div
           key={opt.value}
           className={`FancySelect__Option${opt.value === value ? '--active' : ''}`}
-          onClick={(e) => handleClick(e, idx, options[idx])}
+          onClick={(e) => handleItemClick(e, idx, options[idx])}
         >
           {opt.label}
         </div>
