b136a52c97419a972ac352ab23cb1b1a6eee1454
skylerlee <skyler.ac.lee@gmail.com>
2023-11-29 02:45:06 +0800

    feat: add desc

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h3>Experimental Select Component</h3>
       <article>
-        <FancySelect options={[
-          { label: 'Year', value: 'y' },
-          { label: 'Month', value: 'm' },
-          { label: 'Day', value: 'd' },
-        ]} />
+        <FancySelect
+          options={[
+            { label: 'Year', value: 'y' },
+            { label: 'Month', value: 'm' },
+            { label: 'Day', value: 'd' },
+          ]}
+        />
       </article>
     </div>
   );
