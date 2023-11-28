525fe276bfbe0a57f0c128f3fc17c4a1c13760a3
skylerlee <skyler.ac.lee@gmail.com>
2023-11-29 03:21:30 +0800

    feat: add

--- src/FancySelect.js
+++ src/FancySelect.js
@@ -1,17 +1,27 @@
-import { useControllableValue } from "ahooks";
+import { useControllableValue } from 'ahooks';
 import './FancySelect.css';
 
 function FancySelect(props) {
   const { options = [] } = props;
-  const [ value = options[0]?.value, onChange ] = useControllableValue(props);
+  const [value = options[0]?.value, onChange] = useControllableValue(props);
+
+  const handleClick = (e, idx, opt) => {
+    if (opt !== undefined) {
+      onChange(opt.value);
+    }
+  };
 
   return (
     <div className="FancySelect">
-      {
-        options.map((opt, idx) => (
-          <div key={opt.value} className={`FancySelect__Option${opt.value === value ? '--active' : ''}`}>{opt.label}</div>
-        ))
-      }
+      {options.map((opt, idx) => (
+        <div
+          key={opt.value}
+          className={`FancySelect__Option${opt.value === value ? '--active' : ''}`}
+          onClick={(e) => handleClick(e, idx, options[idx])}
+        >
+          {opt.label}
+        </div>
+      ))}
     </div>
   );
 }
